6c38787524c2d2aa6a6f322411186206cd2980c2
Daniel Clarke <u8sand@gmail.com>
2016-12-17 16:56:21 -0500

    Added brew-travis.sh for mac version of script

---
+++ brew-travis.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Copyright (C) 2016  Mikkel Oscar Lyderik Larsen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Script for setting up and running a travis-ci build in an up to date
+
+
+# read value from .travis.yml
+travis_yml() {
+  ruby -ryaml -e 'puts ARGV[1..-1].inject(YAML.load(File.read(ARGV[0]))) {|acc, key| acc[key] }' .travis.yml $@
+}
+
+read_config() {
+    local old_ifs=$IFS
+    IFS=$'\n'
+    CONFIG_BUILD_SCRIPTS=($(travis_yml brew script))
+    CONFIG_PACKAGES=($(travis_yml brew packages))
+    IFS=$old_ifs
+}
+
+# run build scripts defined in .travis.yml
+build_scripts() {
+  if [ ${#CONFIG_BUILD_SCRIPTS[@]} -gt 0 ]; then
+    for script in "${CONFIG_BUILD_SCRIPTS[@]}"; do
+      $script
+    done
+  else
+    echo "No build scripts defined"
+    exit 1
+  fi
+}
+
+# install packages defined in .travis.yml
+install_packages() {
+  brew update
+  for package in "${CONFIG_PACKAGES[@]}"; do
+    brew install $package
+  done
+}
+
+read_config
+install_packages
+build_scripts
